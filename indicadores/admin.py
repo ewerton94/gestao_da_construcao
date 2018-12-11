17266c3c766ab9471c270959de5a2f5f0c0bac0a
Ewerton Oliveira <ewerton.amorim@live.com>
2018-12-10 22:46:17 -0300

    Register admin

--- indicadores/admin.py
+++ indicadores/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Empresa
+from .models import Empresa, Cliente, Pesquisador, Indicador, Empreendimento, Referencia, TipoIndicador
 
 admin.site.register(Empresa)
+admin.site.register(Empreendimento)
+admin.site.register(Cliente)
+admin.site.register(Pesquisador)
+admin.site.register(Referencia)
+admin.site.register(TipoIndicador)
+admin.site.register(Indicador)
