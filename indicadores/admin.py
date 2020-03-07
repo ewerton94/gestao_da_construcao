4de61b4d77bb7459e26ffad59a254c4e385fc272
Ewerton Oliveira <ewerton.amorim@live.com>
2020-03-07 10:58:14 -0300

    List filter and display resultado admin

--- indicadores/admin.py
+++ indicadores/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Empresa, Cliente, Pesquisador, Resultado, Indicador, Empreendimento, Referencia, TipoIndicador, CampoIndicador, TCPO
 
+class ResultadoAdmin(admin.ModelAdmin):
+    list_display = ('indicador', 'campo_indicador', 'referencia', 'referencia')
+    list_filter = ('empreendimento', 'indicador', 'referencia', 'referencia')
+
 admin.site.register(Empresa)
 admin.site.register(CampoIndicador)
 admin.site.register(Empreendimento)
@@ -9,5 +13,5 @@ admin.site.register(Pesquisador)
 admin.site.register(Referencia)
 admin.site.register(TipoIndicador)
 admin.site.register(Indicador)
-admin.site.register(Resultado)
+admin.site.register(Resultado, ResultadoAdmin)
 admin.site.register(TCPO)
